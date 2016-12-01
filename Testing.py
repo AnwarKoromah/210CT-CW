1dba8de1f0e4cd3cb4ec8045611717a2907bf3e0
AnwarKoromah <koromahanwar@hotmail.co.uk>
2016-12-01 19:46:13 +0000

    Fully functional now

--- Testing.py
+++ Testing.py
@@ -1,5 +1,5 @@
 
-lis = [1,2,3,4,1,2,3,1,2,3,4,5]
+lis = [1,2,4,1,2,1,2,3,4,5]
 longestL = []
 counter = 1
 eList = []
@@ -8,9 +8,11 @@ eList = []
 for i in lis:
         num = int(i)
         print(counter)
+        print(i)
         if counter == len(lis):
                 eList.append(i)
-                break
+                if len(longestL) < len(eList):
+                            longestL = eList
         else:
                 if num < lis[counter]:
                     eList.append(i)
@@ -22,3 +24,14 @@ for i in lis:
         counter = counter + 1
 print(eList)
 print(longestL)
+"""
+eList = []
+a = input("A")
+b = a.split(",")
+print(b)
+for i in b:
+        z = int(i)
+        eList.append(z)
+print(eList)
+
+"""
