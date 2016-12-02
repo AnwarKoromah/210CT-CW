c64c0a3d2142eb93fd150563983ab96df9757d60
AnwarKoromah <koromahanwar@hotmail.co.uk>
2016-12-02 23:33:42 +0000

    Add files via upload

--- Week 5(Task 1).py
+++ Week 5(Task 1).py
@@ -1,77 +1,32 @@
 
-
-a = input("Enter a sequence of integers:")
-lis = list(a)
-
 def longestSubsequence(lis):
     eList = []
     longestL = []
     counter = 1
-"""
-
-    for listL in range(0, len(lis), 1):
-        No = int(listL)
-        for num in lis:
-            if num < lis[No]:
-                print(lis[No])
-                if num in eList:
-                    nothing = True
-                else:
-                    eList.append(num)
-            else:
-                print(lis[No])
-"""
 
     for i in lis:
-        num = int(i)
-        if num < lis[counter]:
-            eList.append(i)
-        else:
-            eList.append(i)
-            longestL = eList
-            eList = []
-    counter = counter + 1
+            num = int(i)
+            if counter == len(lis):
+                    eList.append(i)
+                    if len(longestL) < len(eList):
+                                longestL = eList
+            else:
+                    if num < lis[counter]:
+                        eList.append(i)
+                    else:
+                        eList.append(i)
+                        if len(longestL) < len(eList):
+                                longestL = eList
+                        eList = []
+            counter = counter + 1
+    print("Longest sub-sequence: " + str(longestL))
 
-    print(eList)
-    print(longestL)
-    
-
-longestSubsequence(lis)
-
-
-
-#created the first list
-#append the first ascending sequnce to a new list
-#then have the remaining in another list
-#compare two lists, first is kept under namespace and is the highest.
-#use that to compare to the second sequnce.
-#keep going through sequnces until the end.
-#the second list should be chnaging to the first list if its bigger.
-
-
-
-
-"""
-
-
-#This code below works
 
 eList = []
+a = input("Enter a sequence of numbers sepearted by commas: ")
+b = a.split(",")
+for i in b:
+        z = int(i)
+        eList.append(z)
 
-for listL in range(0, len(lis), 1):
-    No = int(listL)
-    for num in lis:
-        if num < lis[No]:
-            print(num)
-            print(lis[No])
-            print("Success")
-            if num in eList:
-                nothing = True
-            else:
-                eList.append(num)
-        else:
-            print(num)
-            print(lis[No])
-            print("Fail")
-"""
-
+longestSubsequence(eList)
