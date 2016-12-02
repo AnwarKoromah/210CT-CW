fe93b3f3b9690fe4f26aa31aa674c5514bad89de
AnwarKoromah <koromahanwar@hotmail.co.uk>
2016-12-02 23:39:54 +0000

    Tidied up a bit

--- Week 3(Task 2) (1).py
+++ Week 3(Task 2) (1).py
@@ -1,15 +1,6 @@
 #Function to check if a number is a prime number
 
 """
-
-#If num % number below, returns a float then do again for the next number
-#If you get an int returned then stop the loop and say it isn't prime.
-
-#2 is th eonly even prime, so if even then return not prime
-#0 and 1 are not considered prime numbers
-
-#range works like this = range(start, stop, step), step is how to count
-
 for a in range(num-1, 1, -1):
     if num % a == 0:
         b = False
@@ -25,33 +16,20 @@ else:
 """
 
 
-
-
-
-
-
-
-
 def IsPrimeOrNah(num, nextN):
-    if num % nextN == 0:
+    if num == 1:
         print("Nah")
     else:
-        if nextN == 2:
-            print("Prime")
+        if num % nextN == 0:
+            print("Nah")
         else:
-            nextN = nextN-1
-            IsPrimeOrNah(num, nextN)
+            if nextN == 2:
+                print("Prime")
+            else:
+                nextN = nextN-1
+                IsPrimeOrNah(num, nextN)
         
-
-
 
 num = int(input("Please enter a number: "))
 nextN = num-1
 IsPrimeOrNah(num, nextN)
-
-
-
-
-
-
-
