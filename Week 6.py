ae3be2c91bdc21b2838772d74ddf48528e838d38
AnwarKoromah <koromahanwar@hotmail.co.uk>
2016-12-02 23:31:44 +0000

    Add files via upload

---
+++ Week 6.py
@@ -0,0 +1,64 @@
+#include <iostream>
+
+class BinTreeNode(object):
+
+    def __init__(self, value):
+        self.value=value
+        self.left=None
+        self.right=None
+
+
+        
+def tree_insert( tree, item):
+    if tree==None:
+        tree=BinTreeNode(item)
+    else:
+        if(item < tree.value):
+            if(tree.left==None):
+                tree.left=BinTreeNode(item)
+            else:
+                tree_insert(tree.left,item)
+        else:
+            if(tree.right==None):
+                tree.right=BinTreeNode(item)
+            else:
+                tree_insert(tree.right,item)
+    return tree
+
+def postorder(tree):
+    if(tree.left!=None):
+        postorder(tree.left)
+    if(tree.right!=None):
+        postorder(tree.right)
+    print (tree.value)
+  
+
+#function prints the nodes in order
+def in_order(tree):
+    a = True
+    eList = []
+    while a == True:
+        if tree is not None:
+            eList.append(tree)
+            #after the node has been added, it will move left
+            tree = tree.left
+        else:
+            if len(eList) > 0:
+                tree = eList.pop()#removes the most recent value
+                print(tree.value)
+                tree = tree.right
+            else:
+                #to stop the loop
+                a = False
+
+
+if __name__ == '__main__':
+    
+  t=tree_insert(None,6);
+  tree_insert(t,10)
+  tree_insert(t,5)
+  tree_insert(t,2)
+  tree_insert(t,3)
+  tree_insert(t,4)
+  tree_insert(t,11)
+  in_order(t)
