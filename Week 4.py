97946b767a74049c92b6db96b83695670e5ee64e
AnwarKoromah <koromahanwar@hotmail.co.uk>
2016-12-02 23:34:54 +0000

    Turned into function

--- Week 4.py
+++ Week 4.py
@@ -5,6 +5,28 @@ lis = list(a)
 low = int(input("Please enter your low number: "))
 high = int(input("Please enter your high number: "))
 
+eList = []
+
+def searchAlgorithm(lis, high, low, eList):
+    
+    stop = False
+    for i in range(low, high+1, 1):
+        eList.append(i)
+
+    for a in lis:
+        if stop == True:
+            break
+        else:
+            for b in eList:
+                if str(b) == str(a):
+                    print("There are values in the list from the intervals you have provided")
+                    stop = True
+    if stop == False:
+        print("There is no value in the list with the intervals you have provided")
+
+searchAlgorithm(lis, high, low, eList)
+
+"""
 
 #Function that fills in the gap between the low and high
 
@@ -26,5 +48,4 @@ for a in lis:
                 print("True")
                 stop = "Stop"
 
-
-
+"""
